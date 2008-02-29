fb704c9f8e45b9ca34bda25ba5b55d7ef0f7a13b
jason2k <jason2k@79b3db46-2645-0410-bbc9-eb9d3805450a>
2008-02-29 21:48:43 +0000

    Added a folder remotely

---
+++ trunk/source/version.h
@@ -0,0 +1,28 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define SCANTOOL_VERSION          1
+#define SCANTOOL_SUB_VERSION      13
+#define SCANTOOL_WIP_VERSION      0
+
+#ifdef EEEPC_BUILD0
+    #define SCANTOOL_VERSION_STR      "1.13 For EEE-PC Build 1"
+    #define SCANTOOL_VERSION_EX_STR   "1.13 For EEE-PC Build 1"
+    #define SCANTOOL_YEAR_STR         "2008"
+    #define SCANTOOL_DATE             20080229    /* yyyymmdd */
+#else
+    #define SCANTOOL_VERSION_STR      "1.13"
+    #define SCANTOOL_VERSION_EX_STR   "1.13"
+    #define SCANTOOL_YEAR_STR         "2006"
+    #define SCANTOOL_DATE             20060523    /* yyyymmdd */
+#endif
+
+#ifdef ALLEGRO_WINDOWS
+   #define SCANTOOL_PLATFORM_STR   "Windows"
+#else
+   #define SCANTOOL_PLATFORM_STR   "DOS"
+#endif
+
+#define SCANTOOL_COPYRIGHT_STR     "Copyright © " SCANTOOL_YEAR_STR " ScanTool.net, LLC"
+
+#endif
